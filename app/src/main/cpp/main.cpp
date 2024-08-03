d942e6ccc90b6dfa6acdf0d9507c1049c01ea823
BethsFaith <BethsFaith7@gmail.com>
2024-08-03 12:49:12 +0300

    feat(render) : add gles3 and glm libs

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -1,5 +1,11 @@
 #include <jni.h>
 #include <string>
+#include <GLES2/gl2.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+#include <glm/gtx/closest_point.hpp>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_Cube_MainActivity_stringFromJNI(
