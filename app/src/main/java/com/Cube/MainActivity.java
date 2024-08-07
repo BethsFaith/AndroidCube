d5ad75ce716307cdab0327670f9414dded4f56aa
BethsFaith <BethsFaith7@gmail.com>
2024-08-07 19:14:15 +0300

    feat(switch) : add switching between types of rotation

--- app/src/main/java/com/Cube/MainActivity.java
+++ app/src/main/java/com/Cube/MainActivity.java
@@ -1,15 +1,20 @@
 package com.Cube;
 
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.pm.ConfigurationInfo;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.opengl.GLSurfaceView;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -20,7 +25,11 @@ import java.io.InputStreamReader;
 public class MainActivity extends Activity implements View.OnTouchListener {
     private GLSurfaceView glSurfaceView;
     private boolean rendererSet;
+    private Switch swtchB;
+    private float lastX;
+    private float lastY;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,11 +80,33 @@ public class MainActivity extends Activity implements View.OnTouchListener {
                 glSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
             }
 
-            glSurfaceView.setEGLContextClientVersion(2);
+            glSurfaceView.setEGLContextClientVersion(3);
             glSurfaceView.setRenderer(rendererWrapper);
             glSurfaceView.setOnTouchListener(this);
             rendererSet = true;
             setContentView(glSurfaceView);
+
+            this.swtchB = new Switch(this);
+            swtchB.setText("Rotate");
+            swtchB.setTranslationX(10);
+            swtchB.setTextColor(Color.BLACK);
+            addContentView(swtchB, new ActionBar.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT));
+            swtchB.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView,
+                                             boolean isChecked) {
+                    if (isChecked) {
+                        Toast.makeText(MainActivity.this,
+                                        "Rotate mode", Toast.LENGTH_SHORT)
+                                .show();
+
+                    } else {
+                        Toast.makeText(MainActivity.this,
+                                        "Orbit mode", Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                }
+            });
         } else {
             // Should never be seen in production, since the manifest filters
             // unsupported devices.
@@ -110,19 +141,19 @@ public class MainActivity extends Activity implements View.OnTouchListener {
 
     @Override
     protected void onStop() {
-        super.onPause();
+        super.onStop();
 
         if (rendererSet) {
-//            glSurfaceView.onPause();
+            glSurfaceView.onPause();
         }
     }
 
     @Override
     protected void onRestart() {
-        super.onResume();
+        super.onRestart();
 
         if (rendererSet) {
-//            glSurfaceView.onResume();
+            glSurfaceView.onResume();
         }
     }
 
@@ -132,15 +163,13 @@ public class MainActivity extends Activity implements View.OnTouchListener {
             case MotionEvent.ACTION_DOWN: // нажатие
                 break;
             case MotionEvent.ACTION_MOVE: // движение
-                if (lastX < motionEvent.getX()) {
-                    JNIWrapper.round(0, 1, 0);
-                } else if (lastX > motionEvent.getX()) {
-                    JNIWrapper.round(0, -1, 0);
-                }
-                if (lastX < motionEvent.getX() && lastY > motionEvent.getY() ||
-                    lastX > motionEvent.getX() && lastY < motionEvent.getY() ||
-                    lastX < motionEvent.getX() && lastY < motionEvent.getY() ||
-                    lastX > motionEvent.getX() && lastY > motionEvent.getY()) {
+                if (swtchB.isChecked()) {
+                    if (lastX < motionEvent.getX()) {
+                        JNIWrapper.rotate(0, 1, 0);
+                    } else if (lastX > motionEvent.getX()) {
+                        JNIWrapper.rotate(0, -1, 0);
+                    }
+                } else {
                     JNIWrapper.orbit(false);
                 }
                 break;
@@ -154,7 +183,4 @@ public class MainActivity extends Activity implements View.OnTouchListener {
 
         return true;
     }
-
-    private float lastX;
-    private float lastY;
 }
